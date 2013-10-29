e109c41d048a612db02a3995e7b1290605803833
Apprentice <apprentice@dbc17.local>
2013-10-28 19:16:15 -0700

    wrote test for disiplaying item list, added item list to index

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 * User Stories *
 
 * Visitor can post a listing - DONE
-* Visitor can view all listings - IN PROGRESS
-* Visitor can edit their listing via a secret link.
+* Visitor can view all listings - DONE
+* Visitor can edit their listing via edit link. - IN PROGRESS
--- spec/features/user_can_see_item_list_spec.rb
+++ spec/features/user_can_see_item_list_spec.rb
@@ -5,6 +5,15 @@ feature "index of items on homepage" do
 		scenario "user sees list of all items" do
 			create_item(title: 'Andulril', description: 'Great sword, slay enemies well', price: '$5678')
 			expect(page).to have_content 'Andulril'
+			create_item(title: 'Palantir', description: 'Creepy orb that whispers', price: '$5678')
+			expect(page).to have_content 'Palantir'
+			create_item(title: 'Aeglos', description: 'Amazing spear', price: '$5678')
+			expect(page).to have_content 'Aeglos'
+			visit '/'
+			expect(page).to have_content 'Andulril'
+			expect(page).to have_content 'Palantir'
+			expect(page).to have_content 'Aeglos'
+
 
 
 		end
